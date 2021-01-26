e2c07ef950da5ea7eeaf66dd2bad5604459e9eb0
MaryHooker <mc020791@yahoo.com>
2021-01-26 08:23:22 -0600

    (obstacle) trying to generate database for product model

--- test-mvc-webapp/Data/ApplicationDbContext.cs
+++ test-mvc-webapp/Data/ApplicationDbContext.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+//pull in import to access models
+using test_mvc_webapp.Models;
 
 namespace test_mvc_webapp.Data
 {

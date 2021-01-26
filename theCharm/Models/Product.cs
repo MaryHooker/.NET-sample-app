3089ef2f7f27f3a33e6b12e4add0ef0a6d19e2e5
MaryHooker <mc020791@yahoo.com>
2021-01-26 12:08:01 -0600

    working on initial migration for model

---
+++ theCharm/Models/Product.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace theCharm.Models
+{
+    public class Product
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        //get: get value from variable IdProduct
+        //set: set value from variable IdProduct
+        public int IdProduct { get; set; }
+
+        [Display(Name = "Enter Product Code")]
+        [StringLength(20)]
+        [Required(ErrorMessage = "{0} Is required.")]
+
+        public string ProductCode { get; set; }
+
+         [Display(Name = "Enter Product Type")]
+         [StringLength(50)]
+         [Required(ErrorMessage = "{0} Is required.")]
+
+        public string ProductType { get; set; }
+
+         [Display(Name = "Enter Product Description")]
+         [StringLength(150)]
+
+        public string Description { get; set; }
+
+         [Display(Name = "Unit Price")]
+         [DisplayFormat(DataFormatString = "{0:C")]
+        [Required(ErrorMessage = "{0} Is required.")]
+
+        public decimal UnitPrice { get; set; }
+
+         [Display(Name = "Quantity In Stock")]
+         [Required(ErrorMessage = "{0} Is required.")]
+
+        public int QTtyInStock { get; set; }
+
+    }   
+
+}
